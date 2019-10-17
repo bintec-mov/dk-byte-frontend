dfcf2eb98004525f56c5e4b91ff1cc072bd5f8d9
DanielAristy <danielariaty@gmail.com>
2019-10-17 09:00:20 -0500

    Creado menu, home, Login.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MenuComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
